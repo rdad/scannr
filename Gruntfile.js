2f10e93c4e2ad16bc5402eafd3c80a18f51c995d
theo <theo.gilcer@gmail.com>
2014-11-21 16:51:03 +0100

    add watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,11 +27,29 @@ module.exports = function(grunt) {
         dest : 'dist/scannr.min.js'
       },
       options: {}
-    }
+    },
+    watch: {
+      footprint: {
+        files: 'src/footprint/*.js',
+        tasks: ['footprint'],
+        options: {
+          interrupt: true
+        },
+      },
+      app: {
+        files: ['src/*.js', '!src/footprint.js'],
+        tasks: ['default'],
+        options: {
+          interrupt: true
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('footprint', ['concat:footprint', 'default']);
   grunt.registerTask('default', ['concat:global', 'uglify']);
 };
